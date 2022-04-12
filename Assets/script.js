ad7e926afd13b071672697b790224fa136458b5f
Robert Routhier <robrouth92@gmail.com>
2022-04-12 14:26:51 -0700

    variables set

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,26 @@
+var questionIndex = 0;
+var time = questions.lenth * 10;
+
+var questionsEl = document.getElementById("")
+
+var timerEl = document.getElementById("")
+
+var optionsEl = document.getElementById("")
+
+var submitBtn = document.getElementById("submit-btn")
+
+var startBtn = document.getElementById("begin")
+
+var nameEl = document.getElementById("name")
+
+var gradeEl = document.getElementById("grade")
+
+
+
+
+
+
+
 function startGame(){
 
 }
